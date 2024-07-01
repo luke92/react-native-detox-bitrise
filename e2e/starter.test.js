c0c94dd7033307d13eb4785caffb989ef8aacbc2
luke92 <lucasjv92@gmail.com>
2024-07-01 12:19:42 -0300

    first commit

---
+++ e2e/starter.test.js
@@ -0,0 +1,18 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have Step one', async () => {
+    await expect(element(by.text('Step One'))).toBeVisible();
+  });
+
+  it('should show hello screen after tap', async () => {
+    await element(by.id('hello_button')).tap();
+    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  });
+});
