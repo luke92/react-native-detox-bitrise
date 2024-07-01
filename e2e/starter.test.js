3d994a1d72af3c49585a0742d6c6a34c8ed69424
luke92 <lucasjv92@gmail.com>
2024-07-01 17:20:13 -0300

    remove test fail

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -16,9 +16,4 @@ describe('Example', () => {
   it('should have Step one', async () => {
     await expect(element(by.text('Step One'))).toBeVisible();
   });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
 });
