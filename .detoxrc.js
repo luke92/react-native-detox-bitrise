4349efdca868bf62b50c3f880090ada539be6f1c
luke92 <lucasjv92@gmail.com>
2024-09-23 22:15:59 -0300

    add reverse ports

--- .detoxrc.js
+++ .detoxrc.js
@@ -24,6 +24,7 @@ module.exports = {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
       build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
