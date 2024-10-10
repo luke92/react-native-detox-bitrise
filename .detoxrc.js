c4fcfa833e0770243b0ec551c682762e05baee95
luke92 <lucasjv92@gmail.com>
2024-10-10 13:30:08 -0300

    update detox config

--- .detoxrc.js
+++ .detoxrc.js
@@ -2,90 +2,114 @@
 module.exports = {
   testRunner: {
     args: {
-      '$0': 'jest',
-      config: 'e2e/jest.config.js'
+      $0: 'jest',
+      config: 'e2e/jest.config.js',
     },
     jest: {
-      setupTimeout: 120000
-    }
+      setupTimeout: 120000,
+    },
   },
   apps: {
     'ios.debug': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/BitriseReactNative.app',
-      build: 'xcodebuild -workspace ios/BitriseReactNative.xcworkspace -scheme BitriseReactNative -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+      binaryPath:
+        'ios/build/Build/Products/Debug-iphonesimulator/BitriseReactNative.app',
+      build:
+        'xcodebuild -workspace ios/BitriseReactNative.xcworkspace -scheme BitriseReactNative -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'ios.release': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/BitriseReactNative.app',
-      build: 'xcodebuild -workspace ios/BitriseReactNative.xcworkspace -scheme BitriseReactNative -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      binaryPath:
+        'ios/build/Build/Products/Release-iphonesimulator/BitriseReactNative.app',
+      build:
+        'xcodebuild -workspace ios/BitriseReactNative.xcworkspace -scheme BitriseReactNative -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
     },
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
-    }
+      build:
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
+    },
   },
   devices: {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 15'
-      }
+        type: 'iPhone 15',
+      },
     },
     attached: {
       type: 'android.attached',
       device: {
-        adbName: '.*'
-      }
+        adbName: '.*',
+      },
     },
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_34_extension_level_7_arm64-v8a'
-      }
+        avdName: 'Pixel_3a_API_34_extension_level_7_arm64-v8a',
+      },
     },
     bitrise: {
       type: 'android.emulator',
       device: {
-        avdName: 'emulator'
-      }
-    }
+        avdName: 'emulator',
+      },
+    },
+    genycloud: {
+      type: 'android.genycloud',
+      device: {
+        recipeUUID: '45d99fd5-be16-4e02-a81b-6510f3cb7c2e',
+      },
+    },
   },
   configurations: {
     'ios.sim.debug': {
       device: 'simulator',
-      app: 'ios.debug'
+      app: 'ios.debug',
     },
     'ios.sim.release': {
       device: 'simulator',
-      app: 'ios.release'
+      app: 'ios.release',
     },
     'android.att.debug': {
       device: 'attached',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.att.release': {
       device: 'attached',
-      app: 'android.release'
+      app: 'android.release',
     },
     'android.emu.debug': {
       device: 'emulator',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.emu.release': {
       device: 'emulator',
-      app: 'android.release'
+      app: 'android.release',
     },
-    'android.emu.bitrise': {
+    'android.bitrise.debug': {
       device: 'bitrise',
-      app: 'android.debug'
-    }
-  }
+      app: 'android.debug',
+    },
+    'android.bitrise.release': {
+      device: 'bitrise',
+      app: 'android.release',
+    },
+    'android.genycloud.debug': {
+      device: 'genycloud',
+      app: 'android.debug',
+    },
+    'android.genycloud.release': {
+      device: 'genycloud',
+      app: 'android.release',
+    },
+  },
 };
